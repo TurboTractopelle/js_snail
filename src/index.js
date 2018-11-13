9992ff1f195917eba0903536c5c86a492e71a1f8
TurboTractopelle <44016081+TurboTractopelle@users.noreply.github.com>
2018-11-13 23:08:09 +0100

    almost

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-let arr = [[1, 2, 3], [8, 9, 4], [7, 6, 5]];
+let arr = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25]
+];
 
 //console.log(arr[2][2])
 
 function snail(arr) {
   let out = [];
 
-  console.log(arr);
-
   const goRight = (i, j, secure) => {
     if (secure > 0) {
       if (arr[i][j + 1] === undefined) {
         if (out.includes(arr[i][j])) {
-          goDown(i + 1, j - 1, secure);
+          goDown(i + 1, j - 1, --secure);
         } else {
           out.push(arr[i][j]);
           //console.log(out);
@@ -29,9 +33,9 @@ function snail(arr) {
     if (secure > 0) {
       if (arr[i + 1] === undefined) {
         if (out.includes(arr[i][j])) {
-          goLeft(i - 1, j, --secure);
+          goLeft(i - 1, j - 1, --secure);
         } else {
-          goLeft(i, j, --secure);
+          goLeft(i, j, secure);
         }
       } else {
         out.push(arr[i][j]);
@@ -45,7 +49,7 @@ function snail(arr) {
     if (secure > 0) {
       if (arr[i][j - 1] === undefined) {
         if (out.includes(arr[i][j])) {
-          goUp(i - 1, j + 1, --secure);
+          goUp(i - 1, j + 1, secure);
         } else {
           out.push(arr[i][j]);
           //console.log(out);
@@ -63,7 +67,7 @@ function snail(arr) {
     if (secure > 0) {
       if (arr[i - 1] === undefined) {
         if (out.includes(arr[i][j])) {
-          goRight(i + 1, j + 1, --secure);
+          goRight(i + 1, j + 1, secure);
         } else {
           out.push(arr[i][j]);
           //console.log(out);
@@ -78,6 +82,7 @@ function snail(arr) {
   };
   console.log(arr.length);
   if (arr.length > 1) {
+    console.log(arr.length * arr[0].length + 1);
     goRight(0, 0, arr.length * arr[0].length + 1);
     return out;
   } else {
@@ -85,4 +90,4 @@ function snail(arr) {
   }
 }
 
-console.log(snail([[1]]));
+console.log(snail(arr));
