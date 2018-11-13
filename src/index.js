015d442235c9a769f4687550afeb90f772b2e4cf
TurboTractopelle <44016081+TurboTractopelle@users.noreply.github.com>
2018-11-13 21:55:10 +0100

    down

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 let arr = [[1, 2, 3], [8, 9, 4], [7, 6, 5]];
+
+//console.log(arr[2][2])
+
 function snail() {
   const goRight = (i, j, secure) => {
     if (secure > 0) {
       if (arr[i][j + 1] === undefined) {
         console.log(arr[i][j]);
-        goDown(i, j, secure);
+        goDown(i + 1, j, secure);
       } else {
         console.log(arr[i][j]);
         goRight(i, j + 1, --secure);
@@ -14,20 +17,19 @@ function snail() {
 
   const goDown = (i, j, secure) => {
     if (secure > 0) {
-      if (arr[i + 1][j] === undefined) {
+      if (arr[i + 1] === undefined) {
         console.log(arr[i][j]);
-        goLeft(i, j, secure);
+        goLeft(i - 1, j, secure);
       } else {
         console.log(arr[i][j]);
-        goDown(i + 1, j, --secure);
+        goDown(2, j, --secure);
       }
-
-      goDown(i, j, --secure);
     }
   };
 
   const goLeft = (i, j, secure) => {
-    console.log("left");
+    if (secure > 0) {
+    }
   };
 
   goRight(0, 0, 10);
